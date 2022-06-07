54256897cad764812f5a0e26b5b9f9d5c314e778
Mika Reich <mikareich0@gmail.com>
2022-06-07 19:30:39 +0200

    implement function

--- src/ts/getDifficulty.ts
+++ src/ts/getDifficulty.ts
@@ -1,4 +1,8 @@
-import difficulties from './difficulties'
+import {
+  defaultDifficulty,
+  Difficulty,
+  getDifficultyByLevel,
+} from './difficulties'
 
 /**
  * It returns the difficulty level of the game, which is either the difficulty level specified in the
@@ -13,10 +17,9 @@ function getDifficulty() {
   const numberOfCols = Number(searchParams.get('numberOfCols'))
   const numberOfMines = Number(searchParams.get('numberOfMines'))
 
-  const difficulty =
-    Object.values(difficulties).find(
-      (difficulty) => difficulty.level === level
-    ) || difficulties.mediumDifficulty
+  const difficulty = <Difficulty>(
+    getDifficultyByLevel(level || defaultDifficulty.level)
+  )
 
   difficulty.numberOfCols = numberOfCols || difficulty.numberOfCols
   difficulty.numberOfRows = numberOfRows || difficulty.numberOfRows
